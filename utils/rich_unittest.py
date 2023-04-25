9d7ec8b52dba557af44369264fe02afc7ad78fe9
nguyendhst <mountngubinh@gmail.com>
2023-04-25 16:22:47 +0700

    feat: pretty print errors

--- utils/rich_unittest.py
+++ utils/rich_unittest.py
@@ -1,21 +1,29 @@
 import unittest
 from rich.table import Table
+from rich.panel import Panel
 from rich.console import Console
+from rich.traceback import install
+
+
+install()
+
 
 class RichTestResult(unittest.TextTestResult):
     def printErrors(self):
         if self.errors or self.failures:
             console = Console()
-            table = Table(title="Test Failures and Errors")
+            table = Table(title="Test Failures and Errors", style="red")
             table.add_column("Test Case")
             table.add_column("Error")
 
             for test, err in self.errors + self.failures:
-                table.add_row(test.shortDescription() or str(test), err)
+                table.add_row(
+                    test.shortDescription() or str(test),
+                    Panel(err, title="Exception", expand=False),
+                )
 
             console.print(table)
-            
+
 
 class RichTestRunner(unittest.TextTestRunner):
     resultclass = RichTestResult
-    
