087a5cee339a090acb243c422373dc242e28e1b6
nguyendhst <mountngubinh@gmail.com>
2023-04-28 00:59:41 +0700

    feat: `calendar-import/non-data-driven` tests completed

--- features/calendar-import/non-data-driven/script.py
+++ features/calendar-import/non-data-driven/script.py
@@ -32,6 +32,9 @@ FEATURE_URL = "https://sandbox.moodledemo.net/calendar/import.php"
 USERNAME = "admin"
 PASSWORD = "sandbox"
 ICS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "calendar.ics"))
+BAD_ICS_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "yolo.json")
+)
 
 
 class TestCalendarImport(unittest.TestCase):
@@ -78,22 +81,9 @@ class TestCalendarImport(unittest.TestCase):
             print(LOGIN_ERR)
             sys.exit(1)
 
-    # def test_calendar_import(self):
-    #    """Simple access test for the calendar import page"""
-    #    # Go to the calendar import page
-    #    self.driver.get(FEATURE_URL)
-    #    # Check that the page is correct
-    #    self.assertTrue("Import calendar" in self.driver.page_source)
-
-    # def test_calendar_import_ical(self):
-    #    # Go to the calendar import page
-    #    self.driver.get(FEATURE_URL)
-    #    # Check that the page is correct
-    #    self.assertTrue("Import calendar" in self.driver.page_source)
-
-    def test_calendar_valid_field1(self):
+    def test_calendar_normalflow(self):
         """
-        - name: Calendar Import Normal Flow
+        Calendar Import Normal Flow
         - description: Test the calendar import .ics URL functionality with valid fields
         - preconditions: User is logged in
         """
@@ -126,7 +116,7 @@ class TestCalendarImport(unittest.TestCase):
 
     def test_calendar_alt1(self):
         """
-        - name: Calendar Import Alternative Flow 1
+        Calendar Import Alternative Flow 1
         - description: Test the calendar import functionality with File Picker
         - preconditions: User is logged in
         """
@@ -185,52 +175,137 @@ class TestCalendarImport(unittest.TestCase):
         # Check if successfully redirected to the manage calendars page: https://sandbox.moodledemo.net/calendar/managesubscriptions.php
         self.assertTrue("Imported calendars" in self.driver.page_source)
 
-    # def test_calendar_invalid_field1(self):
-    #    """
-    #    - name: Calendar Import Exception Flow 1
-    #    - description: Test the calendar import functionality with Invalid URL field
-    #    - preconditions: User is logged in
-    #    """
+    def test_calendar_badurl_field(self):
+        """
+        Calendar Import Exception Flow 2
+        - description: Test the calendar import functionality with Invalid URL field
+        - preconditions: User is logged in
+        """
 
-    #    # Go to the calendar import page
-    #    self.driver.get(FEATURE_URL)
+        # Go to the calendar import page
+        self.driver.get(FEATURE_URL)
 
-    #    bad_url = "google.com"
-    #    name_field = self.driver.find_element(By.ID, "id_name")
-    #    url_field = self.driver.find_element(By.ID, "id_url")
-    #    submit_btn = self.driver.find_element(By.ID, "id_add")
+        bad_url = "google.com"
+        name_field = self.driver.find_element(By.ID, "id_name")
+        url_field = self.driver.find_element(By.ID, "id_url")
+        submit_btn = self.driver.find_element(By.ID, "id_add")
 
-    #    ActionChains(self.driver).move_to_element(name_field).click().send_keys(
-    #        "Test Calendar"
-    #    ).move_to_element(url_field).click().send_keys(bad_url).move_to_element(
-    #        submit_btn
-    #    ).click().perform()
+        ActionChains(self.driver).move_to_element(name_field).click().send_keys(
+            "Test Calendar"
+        ).move_to_element(url_field).click().send_keys(bad_url).perform()
 
-    #    # verify if a error prompt is shown
-    #    self.assertTrue("Invalid URL" in self.driver.page_source)
+        self.driver.execute_script("arguments[0].click();", submit_btn)
 
-    # def test_calendar_invalid_field2(self):
-    #    """
-    #    - name: Calendar Import Exception Flow 2
-    #    - description: Test the calendar import functionality with Invalid Name field
-    #    - preconditions: User is logged in
-    #    """
+        # verify if a error prompt is shown
+        err = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@id='id_error_url']"))
+        )
+        self.assertTrue(err.is_displayed())
 
-    #    # Go to the calendar import page
-    #    self.driver.get(FEATURE_URL)
+    def test_calendar_empty_field1(self):
+        """
+        Calendar Import Exception Flow 1
+        - description: Test the calendar import functionality with Empty Name field
+        - preconditions: User is logged in
+        """
 
-    #    name_field = self.driver.find_element(By.ID, "id_name")
-    #    url_field = self.driver.find_element(By.ID, "id_url")
-    #    submit_btn = self.driver.find_element(By.ID, "id_add")
+        # Go to the calendar import page
+        self.driver.get(FEATURE_URL)
 
-    #    ActionChains(self.driver).move_to_element(name_field).click().send_keys(
-    #        "Test Calendar"
-    #    ).move_to_element(url_field).click().move_to_element(
-    #        submit_btn
-    #    ).click().perform()
+        submit_btn = self.driver.find_element(By.ID, "id_add")
 
-    #    # verify if a error prompt is shown
-    #    self.assertTrue("Invalid URL" in self.driver.page_source)
+        self.driver.execute_script("arguments[0].click();", submit_btn)
+
+        # verify if a error prompt is shown
+        err = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@id='id_error_name']"))
+        )
+        self.assertTrue(err.is_displayed())
+
+    def test_calendar_empty_field2(self):
+        """
+        Calendar Import Exception Flow 1
+        - description: Test the calendar import functionality with Empty URL field
+        - preconditions: User is logged in
+        """
+
+        # Go to the calendar import page
+        self.driver.get(FEATURE_URL)
+        # Clear the URL field
+        url_field = self.driver.find_element(By.ID, "id_url")
+        url_field.clear()
+
+        # input some text in the name field
+        name_field = self.driver.find_element(By.ID, "id_name")
+        name_field.send_keys("Test Calendar")
+
+        submit_btn = self.driver.find_element(By.ID, "id_add")
+
+        self.driver.execute_script("arguments[0].click();", submit_btn)
+
+        # verify if a error prompt is shown
+        err = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@id='id_error_url']"))
+        )
+
+        self.assertTrue(err.is_displayed())
+
+    def test_calendar_wrong_file_format(self):
+        """
+        Calendar Import Exception Flow 3
+        - description: Test the calendar import functionality with wrong file format
+        - preconditions: User is logged in
+        """
+
+        # Go to the calendar import page
+        self.driver.get(FEATURE_URL)
+
+        # Check that the page is correct
+        self.assertTrue("Import calendar" in self.driver.page_source)
+
+        # Check that the name field is valid
+        calendar_name = self.driver.find_element(By.ID, "id_name")
+        calendar_name.send_keys("Test Calendar Excp 3")
+
+        # Select the file picker option
+        selector = Select(self.driver.find_element(By.ID, "id_importfrom"))
+        selector.select_by_visible_text("Calendar file (.ics)")
+        btn = self.driver.find_element(By.XPATH, "//input[@name='importfilechoose']")
+        btn.click()
+
+        # click the upload file option driver.findElement(By.xpath("//span[contains(@class,'fp-repo-name') and contains(text(), 'Upload a file')]"))
+        btn = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//span[contains(text(), 'Upload a file')]")
+            )
+        )
+        btn.click()
+
+        # wait for 5 seconds for the shitty UI to load
+        time.sleep(5)
+
+        btn = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@name='repo_upload_file']"))
+        )
+        btn.send_keys(BAD_ICS_PATH)
+
+        time.sleep(2)
+
+        btn = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@class='fp-upload-btn btn-primary btn']")
+            )
+        )
+        btn.click()
+
+        time.sleep(2)
+
+        # Check if exception is thrown moodle-exception-message
+        promp_xpath = "//div[contains(text(), 'JSON text filetype cannot be accepted.')]"
+        err = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, promp_xpath))
+        )
+        self.assertTrue(err.is_displayed())
 
 
 if __name__ == "__main__":
