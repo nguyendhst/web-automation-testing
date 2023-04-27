158bb81bb53d24445717b8d21c1ff089c851a77c
nguyendhst <mountngubinh@gmail.com>
2023-04-27 17:45:48 +0700

    refactor: moved most init code to `setUpClass`

--- features/_testdrive/non-data-driven/script.py
+++ features/_testdrive/non-data-driven/script.py
@@ -20,13 +20,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "..", "utils
 from rich_unittest import RichTestRunner
 from logger import Logger
 
-# Set up the driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.implicitly_wait(10)
 
 # Set preferred log level: DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-LOG_LV = "DEBUG"
+LOG_LV = "INFO"
 logger = Logger(LOG_LV)
 
 # Open the page
@@ -34,48 +31,51 @@ LOGIN_URL = "https://sandbox.moodledemo.net/login/index.php"
 USERNAME = "admin"
 PASSWORD = "sandbox"
 
-driver.get(LOGIN_URL)
-
-# Log in
-username = driver.find_element(By.ID, "username")
-password = driver.find_element(By.ID, "password")
-login = driver.find_element(By.ID, "loginbtn")
-
-ActionChains.move_to_element(username) \
-    .click() \
-    .send_keys(USERNAME) \
-    .move_to_element(password) \
-    .click() \
-    .send_keys(PASSWORD) \
-    .move_to_element(login) \
-    .click() \
-    .perform()
-
-# if login failed, exit
-
-if "Available courses" not in driver.page_source:
-    driver.quit()
-    LOGIN_ERR = Panel(
-        f"""
-    Login failed. Please check your credentials and try again.
-    - username: {USERNAME}
-    - password: {PASSWORD}
-    """,
-        title="Login Error",
-        title_align="left",
-        expand=True,
-        style="red",
-    )
-    print(LOGIN_ERR)
-    sys.exit(1)
-
 
 # END OF TEMPLATE -- CREATE YOUR OWN CLASS
 class TestDrive(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        """setUpClass runs once per class.
+        This is where you set up the driver and log in
+        """
+        # Set up the driver
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.implicitly_wait(10)
+
         cls.driver = driver
         cls.logger = logger
+        
+        # Log in
+        driver.get(LOGIN_URL)
+
+        username = driver.find_element(By.ID, "username")
+        password = driver.find_element(By.ID, "password")
+        login = driver.find_element(By.ID, "loginbtn")
+
+        ActionChains(driver).move_to_element(username).click().send_keys(
+            USERNAME
+        ).move_to_element(password).click().send_keys(PASSWORD).move_to_element(
+            login
+        ).click().perform()
+
+        # if login failed, exit
+
+        if "Available courses" not in driver.page_source:
+            driver.quit()
+            LOGIN_ERR = Panel(
+                f"""
+            Login failed. Please check your credentials and try again.
+            - username: {USERNAME}
+            - password: {PASSWORD}
+            """,
+                title="Login Error",
+                title_align="left",
+                expand=True,
+                style="red",
+            )
+            print(LOGIN_ERR)
+            sys.exit(1)
 
     def test_drive(self):
         self.assertTrue("Available courses" in self.driver.page_source)
