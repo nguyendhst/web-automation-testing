9a8bddf8f812258cf988f7b2700f22a2529e7982
Qila8nguyen <otokhung1805@gmail.com>
2023-05-03 22:26:09 +0700

    rebase master

--- utils/logger.py
+++ utils/logger.py
@@ -5,6 +5,13 @@ import logging
 from rich.logging import RichHandler
 
 
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+
 class Logger:
     def __init__(self, level="NOTSET"):
         self.console = Console()
