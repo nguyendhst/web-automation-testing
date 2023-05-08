0521f2475d3077db5016968ef645b65096f8c4f1
Qila8nguyen <otokhung1805@gmail.com>
2023-05-08 14:08:37 +0700

    done non-data-driven feature: create-new-event

--- features/new-event/data-driven/script.py
+++ features/new-event/data-driven/script.py
@@ -22,17 +22,17 @@ LOGIN_URL = "https://sandbox.moodledemo.net/login/index.php"
 FEATURE_URL = "https://sandbox.moodledemo.net/my/"
 USERNAME = "student"
 PASSWORD = "sandbox"
-# href="https://sandbox.moodledemo.net/my/"
+
+DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "dataset.csv"))
 
 class TestCreateNewEvent(unittest.TestCase):  
-#  def __init__(self, methodName: str = ...) -> None:
-#    super().__init__(methodName)
-#    self.login()
-    
   @classmethod
   def setUpClass(cls):
     cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     cls.login(cls)
+
+    if not DATA_PATH:
+      raise Exception("Files needed for tests not found")
   
   def setUp(self):
     self.driver.get(FEATURE_URL)
@@ -41,10 +41,6 @@ class TestCreateNewEvent(unittest.TestCase):
     time.sleep(5)
 
   def login(self):
-    #self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    #driver = self.driver
-
-    #driver.implicitly_wait(10)
     self.driver.get(LOGIN_URL)
 
     username = self.driver.find_element(By.ID, "username")
@@ -54,77 +50,41 @@ class TestCreateNewEvent(unittest.TestCase):
     password.send_keys(PASSWORD)
 
     login = self.driver.find_element(By.ID, "loginbtn").click()
-    #wait = WebDriverWait(driver,timeout=100).until((EC.element_to_be_clickable((By.CSS_SELECTOR, "[href='https://sandbox.moodledemo.net/my/']"))))
-    
-    # dashboard = driver.find_element(By.CSS_SELECTOR, "[href='https://sandbox.moodledemo.net/my/']" ).click()
-    # driver.implicitly_wait(10)
-    #wait = WebDriverWait(driver,timeout=100).until((EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-action='new-event-button']"))))
-    # new_event = driver.find_element(By.CSS_SELECTOR, "[data-action='new-event-button']").click()
 
   def save(self):
     #driver = self.driver
-    save_locator = self.driver.find_element(By.CSS_SELECTOR, "[data-action='save']")
+    save_locator = self.driver.find_element(By.CSS_SELECTOR, '[data-action="save"]')
     save_locator.click()
+
+  def test_no_event_title(self):
+    driver = self.driver
+    self.save()
+    required_error_text = driver.find_element(By.XPATH, '//*[@id="id_error_name"]') 
+    self.assertEqual(True, required_error_text.is_displayed())
 
   def test_with_event_title(self):
     wait = WebDriverWait(self.driver,timeout=100)
     
-    #event_title_input_locator = driver.find_element(By.CSS_SELECTOR, "[id='id_name']")
-    event_title_input_locator = self.driver.find_element(By.XPATH, '//*[@id="id_name"]')
+    event_title_input_locator = self.driver.find_element(By.XPATH, '//input[@id="id_name"]')
     self.driver.implicitly_wait(10)
+    # modal = self.driver.find_element(By.CLASS_NAME, "modal-content")
 
     # sends keys to the input field
     ActionChains(self.driver)\
     .move_to_element(event_title_input_locator)\
-    .send_keys("ABC")\
+    .click(on_element = event_title_input_locator)\
+    .send_keys("ABCD Event")\
     .perform()
-    
     self.save()
-
-    modal = self.driver.find_element(By.CLASS_NAME, "modal-content")
-
-    self.assertEqual(True,modal.is_displayed())
-
-#  def test_no_event_title(self):
-#    driver = self.driver
-#    self.save()
-#    required_error_text = driver.find_element(By.CSS_SELECTOR, "[id='id_error_name']") 
-#    self.assertEqual(True, required_error_text.isDisplayed())
-    
-
-    
+    self.driver.implicitly_wait(10)
+    new_event = 'ABCD Event' in self.driver.find_elements(By.CLASS_NAME, 'eventname')
+  
+    # ariaHidden = modal.get_attribute('aria-hidden')
+    # Đúng là modal không display...
+    self.assertEqual(True, new_event)
 
 
-#class TestCreateNewEvent (StudentTesting):
-#  def test_with_event_title(self):
-#    driver = self.driver
-#    wait = WebDriverWait(driver,timeout=100)
-    
-#    #event_title_input_locator = driver.find_element(By.CSS_SELECTOR, "[id='id_name']")
-#    event_title_input_locator = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='id_name']")))
-
-#    ActionChains(driver)\
-#      .move_to_element(event_title_input_locator)\
-#      .send_keys("ABC")
-#    self.save()
-
-#    modal = driver.find_element(By.CLASS_NAME, "modal-content")
-
-#    self.assertEqual(True,modal.isDisplayed())
-
-#  def test_no_event_title(self):
-#    driver = self.driver
-#    self.save()
-#    required_error_text = driver.find_element(By.CSS_SELECTOR, "[id='id_error_name']") 
-#    self.assertEqual(True, required_error_text.isDisplayed())
-
-  # def test_with_event_title(self):
-  #   driver = self.driver
-  #   event_title_input_locator = driver.find_element(By.CSS_SELECTOR, "[id='id_name']")
-  #   ActionChains(driver)\
-  #     .move_to_element(event_title_input_locator)\
-  #     .send_keys("ABC")
-  #   self.save()
+  
 
 if __name__ == "__main__":
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestCreateNewEvent)
